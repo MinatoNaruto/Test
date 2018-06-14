6ea96578e36d2ae4d645dbdd6e3111d9586be7d7
MinatoNaruto <1085271230@qq.com>
2018-06-14 14:30:43 +0800

    idea  first

---
+++ src/java/com/baidu/firstClass.java
@@ -0,0 +1,16 @@
+package com.baidu;
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* User: wangcl
+ * \* Date: 2018/6/14
+ * \* Time: 14:29
+ * \* To change this template use File | Settings | File Templates.
+ * \* Description:
+ * \
+ */
+public class firstClass {
+    public static void main(String[] args) {
+        System.out.println("111111111111");
+    }
+}
