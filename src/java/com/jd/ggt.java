b36d4db517360e5ba13fcc729df47f73cd2c3670
MinatoNaruto <1085271230@qq.com>
2018-06-14 14:47:02 +0800

    为

---
+++ src/java/com/jd/ggt.java
@@ -0,0 +1,13 @@
+package com.jd;
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* User: wangcl
+ * \* Date: 2018/6/14
+ * \* Time: 14:46
+ * \* To change this template use File | Settings | File Templates.
+ * \* Description:
+ * \
+ */
+public class ggt {
+}
