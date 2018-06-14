051a13487cb2b820333bd817421a0581cec2d951
MinatoNaruto <1085271230@qq.com>
2018-06-14 14:34:18 +0800

    idea  first

---
+++ src/java/com/jd/secondClass.java
@@ -0,0 +1,16 @@
+package com.jd;
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* User: wangcl
+ * \* Date: 2018/6/14
+ * \* Time: 14:33
+ * \* To change this template use File | Settings | File Templates.
+ * \* Description:
+ * \
+ */
+public class secondClass {
+    public static void main(String[] args) {
+        System.out.println("tow");
+    }
+}
